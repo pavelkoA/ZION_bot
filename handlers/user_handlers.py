fd6afdafafbf48dee150ee2b07509191be730675
pavelkoA <115475612+pavelkoA@users.noreply.github.com>
2023-03-22 22:25:41 +0300

    inline_buttonpopivu

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,8 +4,14 @@ from aiogram.filters import Text, Command
 from aiogram.methods.get_chat_administrators import GetChatAdministrators
 import bot
 
+from keyboards.keyboards import kb_popivu
+
 router: Router = Router()
 
-@router.message(Command(commands=['admins']))
-async def process_start_command(message: Message):
-    await message.answer(text=str(get_chat_admins(bot, message.chat.id)))
+# @router.message(Command(commands=['admins']))
+# async def process_start_command(message: Message):
+#     await message.answer(text=str(get_chat_admins(bot, message.chat.id)))
+
+@router.message(Command(commands=['popivy']))
+async def process_popivu_commands(message: Message):
+    await message.answer(text='Ну наконец здравое предложение!!!', reply_markup=kb_popivu)
---
+++ keyboards/keyboards.py
@@ -0,0 +1,5 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+kb_popivu: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='ДАААА', callback_data='yes')],
+                                                                        [InlineKeyboardButton(text='Я душнила', callback_data='no')]])
+
