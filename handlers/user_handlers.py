af615b17189e8263e79d66ae5bd491e4d92aa92a
pavelkoA <115475612+pavelkoA@users.noreply.github.com>
2023-03-23 22:32:08 +0300

    help

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,17 +1,15 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Text, Command
-from aiogram.handlers import PollHandler
-from aiogram.methods.get_chat_administrators import GetChatAdministrators
-import bot
 
 from keyboards.keyboards import kb_popivu, kb_opros
+from lexicon.lexicon_ru import words
 
 router: Router = Router()
 
-# @router.message(Command(commands=['admins']))
-# async def process_start_command(message: Message):
-#     await message.answer(text=str(get_chat_admins(bot, message.chat.id)))
+@router.message(Command(commands=['help']))
+async def process_start_command(message: Message):
+    await message.answer(text=words['help'])
 
 @router.message(Command(commands=['popivy']))
 async def process_popivu_commands(message: Message):
