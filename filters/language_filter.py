2f21a61db390660a86d1f74f7492495a8c3feb23
pavelkoA <115475612+pavelkoA@users.noreply.github.com>
2023-03-24 11:11:48 +0300

    ___

--- filters/language_filter.py
+++ filters/language_filter.py
@@ -1,17 +1,17 @@
 from aiogram import F
 from aiogram.types import Message
-from lexicon.lexicon_ru import forbidden_words, sbor
+from lexicon.lexicon_ru import words
 
 #Функция возвращает True если в сообщениях встречается запрещенное в чате слово
 def forbid_wrds(message: Message) -> bool:
-    for word in forbidden_words:
+    for word in words['forbidden_words']:
         if word in message.text.lower():
             return True
     return False
 
 #Функция возвращает True если в сообщение встречается призыв собраться
 def sbor_wrds(message: Message) -> bool:
-    for word in sbor:
+    for word in words['sbor']:
         if word in message.text.lower():
             return True
     return False
